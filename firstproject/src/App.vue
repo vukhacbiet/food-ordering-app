37c3032c029815c14d60b631e54e6ca6a3f0aebd
doanjulia <dathaodoanngoc310@gmail.com>
2022-05-18 16:47:42 +0700

    mytemplate.vue created

--- firstproject/src/App.vue
+++ firstproject/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <Template/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTemp from './components/mytemplate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTemp
   }
 }
 </script>
---
+++ firstproject/src/components/mytemplate.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 if="heading">Hello Testing Hello</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyTemp",
+    // Data
+    // Lifecycles
+    //Methods
+}
+</script>
+<style>
+#heading{
+    color: brown;
+}
+</style>
