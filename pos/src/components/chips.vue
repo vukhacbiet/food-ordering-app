1649733b0a0a3e3971cbffa6db0ab1cdc4371acb
VuDepTrai <vungo246@gmail.com>
2022-05-25 09:47:16 +0700

    Updated

--- pos/src/components/chips.vue
+++ pos/src/components/chips.vue
@@ -1,66 +1,47 @@
 <template>
-    <div>
-      <v-container style="margin-left: 20px">
+<div>
+    <v-container>
         <v-row>
-    <v-col
-      cols="12"
-      sm="20"
-      md="10"
-    >
-      
-        <v-chip-group
-          mandatory
-          active-class="primary--text">
-            <v-chip
-            class="ma-2"
-              v-for="tag in tags"
-              :key="tag"
-            >
-              {{ tag }}
-            </v-chip>
-            
-        </v-chip-group>
-        </v-col>
+            <v-col sm="12" md="12" lg="8">
+                <v-chip-group mandatory active-class="primary--text">
+                    <v-chip class="ma-2" v-for="tag in tags" :key="tag">
+                        {{ tag }}
+                    </v-chip>
+                </v-chip-group>
+            </v-col>
+            <v-col align="right" sm="12" md="12" lg="4">
+              <v-autocomplete v-model="values" outlined dense label="Lorem ipsum"></v-autocomplete>
+          </v-col>
         </v-row>
 
-      <v-col
-      align="right"
-      cols="2"
-      sm="2"
-      md="3">
-      <v-autocomplete
-              v-model="values"
-              outlined
-              dense
-              label="Lorem ipsum"></v-autocomplete>
-      </v-col>
-  </v-container>
+        
+    </v-container>
 
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-    name:'myChips',
+    name: 'myChips',
     data: () => ({
         tags: [
-        'Sapien',
-        'El punto',
-        'Textos',
-        'Contrario',
-        'Contenido',
-        'Sólo',
-        'Esencialmente',
-        'Phasellus',
-        'Sapien',
-        'El punto',
-        'Textos',
-        'Contrario',
-        'Contenido',
-        'Sólo',
-        'Esencialmente',
-        'Phasellus',
-      ]
+            'Sapien',
+            'El punto',
+            'Textos',
+            'Contrario',
+            'Contenido',
+            'Sólo',
+            'Esencialmente',
+            'Phasellus',
+            'Sapien',
+            'El punto',
+            'Textos',
+            'Contrario',
+            'Contenido',
+            'Sólo',
+            'Esencialmente',
+            'Phasellus',
+        ]
     })
 }
 </script>
